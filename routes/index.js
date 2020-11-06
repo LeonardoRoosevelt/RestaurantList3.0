007ee2e5d4a58fd14088831e347e9ea4f3982563
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-11-06 21:26:10 +0800

    feat: add user modules & registation

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 const home = require('./modules/home')
-router.use('/', home)
-
-const search = require('./modules/search')
-router.use('/search', search)
-
 const restaurants = require('./modules/restaurants')
-router.use('/restaurants', restaurants)
-
+const search = require('./modules/search')
 const sort = require('./modules/sort')
+const users = require('./modules/users')
+
+router.use('/', home)
+router.use('/restaurants', restaurants)
+router.use('/search', search)
 router.use('/sort', sort)
+router.use('/users', users)
 
 module.exports = router
