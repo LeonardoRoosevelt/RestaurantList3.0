57133c4c9f3500f0902440790fce4e846f5a15ad
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-11-06 21:55:39 +0800

    feat: add express-session & dotenv

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 //server and database
 const app = express()
 require('./config/mongoose')
 const routes = require('./routes')
-const port = 3000
+const PORT = process.env.PORT
 
 // Set middlewares
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
@@ -17,8 +22,16 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+)
+
 app.use(routes)
 
-app.listen(port, () => {
-  console.log(`listening on http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`listening on http://localhost:${PORT}`)
 })
