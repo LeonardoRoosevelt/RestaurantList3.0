4509266a992ef1ef1a1c2ac59d7ccaa1daa1bf7b
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-11-07 12:28:47 +0800

    feat: add facebook login & dotenv

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,15 @@ const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
 const sort = require('./modules/sort')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
+
 const { authenticator } = require('../middleware/auth') // 掛載 middleware
 
 router.use('/restaurants', authenticator, restaurants)
 router.use('/search', search)
 router.use('/sort', sort)
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
